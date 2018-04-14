ba170958cd02d84df2040c1b620b3f72221d6cbc
AniketBajpai <quantum.computing96@gmail.com>
2018-04-15 01:22:04 +0530

    Added test for refine embeddings 2

--- refine_embeddings2.py
+++ refine_embeddings2.py
@@ -1,3 +1,5 @@
+import better_exceptions
+
 import numpy as np
 import tensorflow as tf
 
@@ -7,32 +9,32 @@ class EmbeddingsRefiner(object):
     """ Class to refine embeddings before matching
     """
 
-    def __init__(self):
+    def __init__(self, embedding_dimensions=128):
         self.num_refinement_steps = 5
         self.use_left_refinement = True
         self.use_right_refinement = True
-        self.embedding_dimensions = 128
+        self.embedding_dimensions = embedding_dimensions
 
-    def refine(self, left_hypercolumns, right_hypercolumns):
+    def refine(self, left_hypercolumns, right_hypercolumns, L):
         """ refine hypercolumn embeddings of both left and right image
-        :param left_hypercolumns - length L list of [batch_size, 128] tensors
+        :param left_hypercolumns - [L, batch_size, 128]
         :param right_hypercolumns - length L list of [batch_size, 128] tensors
         """
         right_features_refined = self.fce_right(right_hypercolumns) # (L, batch_size, 128)
-        left_features_refined = self.fce_left(left_hypercolumns, right_features_refined)
+        left_features_refined = self.fce_left(left_hypercolumns, right_features_refined, L)
 
         return left_features_refined, right_features_refined
 
-    def fce_left(self, left_hypercolumns, right_hypercolumns):
+    def fce_left(self, left_hypercolumns, right_hypercolumns, L):
         """ refine hypercolumn for left image
         f(x_i, S) = attLSTM(f'(x_i), g(S), K)
         hypercolumn refinement is done by running LSTM for fixed no. of steps (num_refinement_steps)
         attention over hypercolumns of points on epipolar line in right image used as
         context vector of LSTM
-        :param left_hypercolumn - [batch_size, 128] tensor (point feature)
+        :param left_hypercolumns - [L, batch_size, 128]
         :param right_hypercolumns - [L, batch_size, 128]
         """
-        L = len(left_hypercolumns)
+        batch_size = tf.shape(left_hypercolumns)[1]
 
         cells = [None] * L
         prev_states = [None] * L
@@ -42,9 +44,9 @@ class EmbeddingsRefiner(object):
             cells[i] = rnn.BasicLSTMCell(self.embedding_dimensions)
             prev_states[i] = cells[i].zero_state(batch_size, tf.float32)   # state[0] is c, state[1] is h
 
-         for step in xrange(self.num_refinement_steps):
-             for i in range(L):
-                outputs[i], state = cells[i](left_hypercolumn, prev_state)  # output: (batch_size, 128)
+        for step in xrange(self.num_refinement_steps):
+            for i in range(L):
+                outputs[i], state = cells[i](left_hypercolumns[i], prev_states[i])  # output: (batch_size, 128)
 
                 h_k = tf.add(outputs[i], left_hypercolumns[i]) # (batch_size, 128)
 
@@ -71,3 +73,33 @@ class EmbeddingsRefiner(object):
 
         right_features_refined = tf.add(tf.stack(right_hypercolumns), tf.stack(outputs))
         return right_features_refined
+
+
+def main():
+    embedding_dimensions = 32
+    refiner = EmbeddingsRefiner(embedding_dimensions=embedding_dimensions)
+
+    sess = tf.InteractiveSession()
+    L = 10
+    batch_size = 4
+
+    left_hypercolumns = tf.constant(np.random.randn(L, batch_size, embedding_dimensions), dtype=tf.float32)
+    right_hypercolumns = [None] * L
+    for i in xrange(L):
+        right_hypercolumns[i] = tf.constant(np.random.randn(batch_size, embedding_dimensions), dtype=tf.float32)
+
+    left_features_refined, right_features_refined = refiner.refine(left_hypercolumns, right_hypercolumns, L)
+    sess.run(tf.global_variables_initializer())
+
+    left_fs, right_fs = sess.run([left_features_refined, right_features_refined])
+
+    print(len(left_fs))
+    print(left_fs[0].shape)
+    print(len(right_fs))
+    print(right_fs[0].shape)
+
+    sess.close()
+
+
+if __name__ == '__main__':
+    main()
